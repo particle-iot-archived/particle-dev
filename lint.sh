8eef70c537d91ec42e843fbddd1aed47b175aee9
Wojtek Siudzinski <admin@suda.pl>
2014-08-05 12:11:04 +0200

    Added script for linting CoffeeScript files

---
+++ lint.sh
@@ -0,0 +1,4 @@
+echo "Linting code..."
+coffee-jshint --globals atom,module,require,console lib/*.coffee
+echo "Linting tests..."
+coffee-jshint --globals require,atom,describe,it,expect,beforeEach,afterEach,waitsForPromise,waitsFor,runs,console spec/*.coffee
