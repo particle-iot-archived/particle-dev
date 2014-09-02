e42c5aa891107845a8a6f1de8ec42b19e8dbe667
Wojtek Siudzinski <admin@suda.pl>
2014-09-02 17:02:00 +0200

    Updated linter

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
 echo "Linting code..."
-find lib/ -name '*.coffee' | xargs coffee-jshint --globals atom,module,require,setTimeout,clearTimeout,setInterval,clearInterval,window
+find lib/ -name '*.coffee' | xargs coffee-jshint --globals atom,module,require,setTimeout,clearTimeout,setInterval,clearInterval,window,localStorage,__dirname
 echo "Linting tests..."
-find spec/ -name '*.coffee' | xargs coffee-jshint --globals atom,module,require,setTimeout,clearTimeout,setInterval,clearInterval,window,describe,it,expect,beforeEach,afterEach,waitsForPromise,waitsFor,runs,spyOn,jasmine
+find spec/ -name '*.coffee' | xargs coffee-jshint --globals atom,module,require,setTimeout,clearTimeout,setInterval,clearInterval,window,localStorage,__dirname,describe,it,expect,beforeEach,afterEach,waitsForPromise,waitsFor,runs,spyOn,jasmine
