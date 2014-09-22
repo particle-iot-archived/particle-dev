743b6e688dcdd213a88dfdfbdd7360341ab3e2a6
Wojtek Siudzinski <admin@suda.pl>
2014-09-22 16:51:08 +0200

    Installing packages

--- build/mac/build.sh
+++ build/mac/build.sh
@@ -4,10 +4,18 @@ BUILD=$(cd "$(dirname "$0")"; pwd)
 ROOT=$(cd "$(dirname "$0")"; cd ../..; pwd)
 TARGET="${ROOT}/dist/mac"
 
-# if [ -d $TARGET ]; then rm -r $TARGET ; fi
+install_package () {
+  cd ${TARGET}/node_modules
+  git clone --depth=1 $1 $2
+  cd $2
+  rm -rf .git
+  npm install
+}
+
+if [ -d $TARGET ]; then rm -r $TARGET ; fi
 mkdir -p $TARGET
 cd $TARGET
-# git clone https://github.com/atom/atom.git .
+git clone --depth=1 https://github.com/atom/atom.git .
 
 # Copy resources
 cp ${BUILD}/sparkide.icns ${TARGET}/resources/mac/atom.icns
@@ -17,5 +25,7 @@ patch ${TARGET}/resources/mac/atom-Info.plist < ${BUILD}/atom-Info.patch
 patch ${TARGET}/src/browser/atom-application.coffee < ${BUILD}/atom-application.patch
 patch ${TARGET}/src/atom.coffee < ${BUILD}/atom.patch
 patch ${TARGET}/.npmrc < ${BUILD}/npmrc.patch
+patch ${TARGET}/package.json < ${BUILD}/package.patch
 
-# script/build --install-dir="${TARGET}/Spark IDE.app"
+cd $TARGET
+script/build --install-dir="${TARGET}/Spark IDE.app"
