e9e0429c5b6844056013042bdafc1c3cae32d8ea
Wojtek Siudzinski <admin@suda.pl>
2014-09-23 18:40:17 +0200

    Building DMG

--- build/mac/build.sh
+++ build/mac/build.sh
@@ -3,6 +3,9 @@
 BUILD=$(cd "$(dirname "$0")"; pwd)
 ROOT=$(cd "$(dirname "$0")"; cd ../..; pwd)
 TARGET="${ROOT}/dist/mac"
+APP_NAME="Spark IDE"
+TEMP_DIR=`mktemp -d tmp.XXXXXXXXXX`
+TEMP_DIR="${BUILD}/${TEMP_DIR}"
 
 install_package () {
   cd ${TARGET}/node_modules
@@ -12,20 +15,38 @@ install_package () {
   npm install
 }
 
-if [ -d $TARGET ]; then rm -r $TARGET ; fi
+if [ -d $TARGET ]; then rm -rf $TARGET ; fi
 mkdir -p $TARGET
-cd $TARGET
+cd $TEMP_DIR
 git clone --depth=1 https://github.com/atom/atom.git .
 
 # Copy resources
-cp ${BUILD}/sparkide.icns ${TARGET}/resources/mac/atom.icns
+cp ${BUILD}/sparkide.icns ${TEMP_DIR}/resources/mac/atom.icns
 
 # Patch code
-patch ${TARGET}/resources/mac/atom-Info.plist < ${BUILD}/atom-Info.patch
-patch ${TARGET}/src/browser/atom-application.coffee < ${BUILD}/atom-application.patch
-patch ${TARGET}/src/atom.coffee < ${BUILD}/atom.patch
-patch ${TARGET}/.npmrc < ${BUILD}/npmrc.patch
-patch ${TARGET}/package.json < ${BUILD}/package.patch
+patch ${TEMP_DIR}/resources/mac/atom-Info.plist < ${BUILD}/atom-Info.patch
+patch ${TEMP_DIR}/src/browser/atom-application.coffee < ${BUILD}/atom-application.patch
+patch ${TEMP_DIR}/src/atom.coffee < ${BUILD}/atom.patch
+patch ${TEMP_DIR}/.npmrc < ${BUILD}/npmrc.patch
+patch ${TEMP_DIR}/package.json < ${BUILD}/package.patch
+#
+cd $TEMP_DIR
+script/build --install-dir="${TARGET}/${APP_NAME}.app"
+rm -rf $TEMP_DIR
 
-cd $TARGET
-script/build --install-dir="${TARGET}/Spark IDE.app"
+# Build DMG
+TEMPLATE="${HOME}/tmp/template.dmg"
+WC_DMG="${TARGET}/image.dmg"
+MASTER_DMG="${TARGET}/Spark IDE.dmg"
+WC_DIR="${TARGET}/image"
+cp $TEMPLATE $WC_DMG
+mkdir -p $WC_DIR
+hdiutil attach $WC_DMG -noautoopen -quiet -mountpoint $WC_DIR
+rm -rf "${WC_DIR}/${APP_NAME}.app"
+ditto -rsrc "${TARGET}/${APP_NAME}.app" "${WC_DIR}/${APP_NAME}.app"
+WC_DEV=`hdiutil info | grep "${WC_DIR}" | grep "/dev/disk" | awk '{print $1}'`
+hdiutil detach $WC_DEV -quiet -force
+rm -f $MASTER_DMG
+hdiutil convert "${WC_DMG}" -quiet -format UDZO -imagekey zlib-level=9 -o "${MASTER_DMG}"
+rm -rf $WC_DIR
+rm -f $WC_DMG
