f5b63f3ef44cd8a23b4b0347606aeed452da8174
Wojtek Siudzinski <admin@suda.pl>
2014-10-08 16:00:58 +0200

    Adding 3rd party packages without patching package.json

--- build/mac/build.sh
+++ build/mac/build.sh
@@ -18,6 +18,7 @@ install_package () {
 if [ -d $TARGET ]; then rm -rf $TARGET ; fi
 mkdir -p $TARGET
 cd $TEMP_DIR
+echo "Working directory is ${TEMP_DIR}"
 git clone --depth=1 https://github.com/atom/atom.git .
 
 # Copy resources
@@ -28,11 +29,33 @@ patch ${TEMP_DIR}/resources/mac/atom-Info.plist < ${BUILD}/atom-Info.patch
 patch ${TEMP_DIR}/src/browser/atom-application.coffee < ${BUILD}/atom-application.patch
 patch ${TEMP_DIR}/src/atom.coffee < ${BUILD}/atom.patch
 patch ${TEMP_DIR}/.npmrc < ${BUILD}/npmrc.patch
-patch ${TEMP_DIR}/package.json < ${BUILD}/package.patch
-#
+
 cd $TEMP_DIR
+# Bootstrap Atom
+script/bootstrap
+
+echo "Installing 3rd party packages"
+export ATOM_HOME=$TEMP_DIR
+./apm/node_modules/atom-package-manager/bin/apm install language-arduino
+./apm/node_modules/atom-package-manager/bin/apm install file-type-icons
+./apm/node_modules/atom-package-manager/bin/apm install switch-header-source
+./apm/node_modules/atom-package-manager/bin/apm install resize-panes
+./apm/node_modules/atom-package-manager/bin/apm install maximize-panes
+./apm/node_modules/atom-package-manager/bin/apm install move-panes
+./apm/node_modules/atom-package-manager/bin/apm install swap-panes
+mv packages/* node_modules/
+unset ATOM_HOME
+
+echo "Installing Spark IDE package"
+git clone --depth=1 git@github.com:spark/spark-ide.git node_modules/spark-ide
+cd node_modules/spark-ide
+export ATOM_NODE_VERSION="0.17.1"
+../../apm/node_modules/atom-package-manager/bin/apm install .
+unset ATOM_NODE_VERSION
+cd ../..
+
 script/build --install-dir="${TARGET}/${APP_NAME}.app"
-rm -rf $TEMP_DIR
+# rm -rf $TEMP_DIR
 
 # Build DMG
 TEMPLATE="${HOME}/tmp/template.dmg"
