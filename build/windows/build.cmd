f810ab62d675de49de71dcd6a49d9f2a2a8afd70
Wojtek Siudzinski <admin@suda.pl>
2014-10-28 19:05:10 +0100

    Fixed building

--- build/windows/build.cmd
+++ build/windows/build.cmd
@@ -30,9 +30,10 @@ node %COMMON%\append-package %TEMP_DIR%\package.json resize-panes "0.1.0"
 node %COMMON%\append-package %TEMP_DIR%\package.json maximize-panes "0.1.0"
 node %COMMON%\append-package %TEMP_DIR%\package.json move-panes "0.1.2"
 node %COMMON%\append-package %TEMP_DIR%\package.json swap-panes "0.1.0"
-node %COMMON%\append-package %TEMP_DIR%\package.json toolbars "0.0.3"
+node %COMMON%\append-package %TEMP_DIR%\package.json toolbar "0.0.5"
 node %COMMON%\append-package %TEMP_DIR%\package.json monokai "0.8.0"
 node %COMMON%\append-package %TEMP_DIR%\package.json welcome
+node %COMMON%\append-package %TEMP_DIR%\package.json feedback-view
 
 echo "Bootstrap Atom"
 script/bootstrap
