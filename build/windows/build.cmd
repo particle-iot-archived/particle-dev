04877048ebe242915b563442e382c70063a10e9d
Wojtek Siudzinski <admin@suda.pl>
2014-10-30 16:52:38 +0100

    toolbar@0.0.9

--- build/windows/build.cmd
+++ build/windows/build.cmd
@@ -31,7 +31,7 @@ node %COMMON%\append-package %TEMP_DIR%\package.json resize-panes "0.1.0"
 node %COMMON%\append-package %TEMP_DIR%\package.json maximize-panes "0.1.0"
 node %COMMON%\append-package %TEMP_DIR%\package.json move-panes "0.1.2"
 node %COMMON%\append-package %TEMP_DIR%\package.json swap-panes "0.1.0"
-node %COMMON%\append-package %TEMP_DIR%\package.json toolbar "0.0.8"
+node %COMMON%\append-package %TEMP_DIR%\package.json toolbar "0.0.9"
 node %COMMON%\append-package %TEMP_DIR%\package.json monokai "0.8.0"
 node %COMMON%\append-package %TEMP_DIR%\package.json welcome
 node %COMMON%\append-package %TEMP_DIR%\package.json feedback
