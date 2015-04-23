92d84548af30d095fd36aec6fb409e233285aa8d
Wojtek Siudzinski <admin@suda.pl>
2015-04-23 17:15:34 +0100

    Updated CI build script

--- build/build-package.sh
+++ build/build-package.sh
@@ -2,24 +2,43 @@
 
 source .atomrc
 
-echo "Downloading node v0.10.22..."
-curl -s -O http://nodejs.org/dist/v0.10.22/node-v0.10.22-darwin-x64.tar.gz
-tar -zxf node-v0.10.22-darwin-x64.tar.gz
-export PATH=$PATH:$PWD/node-v0.10.22-darwin-x64/bin
-
-echo "Downloading latest Atom release..."
+echo "Downloading Atom ${ATOM_VERSION} release..."
 curl -s -L "https://github.com/atom/atom/releases/download/${ATOM_VERSION}/atom-mac.zip" \
   -H 'Accept: application/octet-stream' \
   -o atom.zip
 
 mkdir atom
 unzip -q atom.zip -d atom
+export PATH=$PWD/atom/Atom.app/Contents/Resources/app/apm/bin:$PATH
 
 echo "Using Atom version:"
 ATOM_PATH=./atom ./atom/Atom.app/Contents/Resources/app/atom.sh -v
 
 echo "Downloading package dependencies..."
-atom/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm update
+atom/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm clean
+atom/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm install
+
+TEST_PACKAGES="${APM_TEST_PACKAGES:=none}"
+
+if [ "$TEST_PACKAGES" != "none" ]; then
+  echo "Installing atom package dependencies..."
+  for pack in $TEST_PACKAGES ; do
+    atom/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm install $pack
+  done
+fi
+
+if [ -f ./node_modules/.bin/coffeelint ]; then
+  if [ -d ./lib ]; then
+    echo "Linting package..."
+    ./node_modules/.bin/coffeelint lib
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+  fi
+  if [ -d ./spec ]; then
+    echo "Linting package specs..."
+    ./node_modules/.bin/coffeelint spec
+    rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+  fi
+fi
 
 echo "Running specs..."
 ATOM_PATH=./atom atom/Atom.app/Contents/Resources/app/apm/node_modules/.bin/apm test --path atom/Atom.app/Contents/Resources/app/atom.sh
