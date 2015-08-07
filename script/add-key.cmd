ca7b6fe4e86e05f6dc58d96d830944534e9da9f0
Wojtek Siudzinski <admin@suda.pl>
2015-08-07 16:00:17 +0100

    Fix adding key script

--- script/add-key.cmd
+++ script/add-key.cmd
@@ -1,3 +1,2 @@
-#!/bin/bash
-openssl aes-256-cbc -k %ENCRYPTION_SECRET% -in .\build\resources\authenticode-signing-cert.p12.enc -out .\build\resources\authenticode-signing-cert.p12 -a
+openssl aes-256-cbc -k %ENCRYPTION_SECRET% -in .\build\resources\authenticode-signing-cert.p12.enc -out .\build\resources\authenticode-signing-cert.p12 -d -a
 certutil -p %KEY_PASSWORD% -importpfx .\build\resources\authenticode-signing-cert.p12
