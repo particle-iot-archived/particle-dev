9bac3de3072785a327889332207c52924dbc1955
Wojtek Siudzinski <admin@suda.pl>
2017-04-04 19:10:25 +0100

    Add unit tests

---
+++ test/contexts/compilation.spec.js
@@ -0,0 +1,47 @@
+'use babel';
+
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import chai from 'chai';
+chai.use(sinonChai);
+
+import {expect} from 'chai';
+import path from 'path';
+
+let compilationContext = require('../../lib/contexts/compilation')
+
+describe('Compilation context', () => {
+	let files = [
+		'foo.ino',
+		'../src/bar.cpp',
+		'../src/bar.h'
+	];
+	let root = path.resolve(path.join(__dirname, '..', 'data', 'libraryexample', 'examples'));
+
+	describe('mapCommonPrefix', () => {
+		it('prefixes the files', () => {
+			let result = compilationContext.mapCommonPrefix(files, root);
+			expect(result).to.eql({
+				'examples/foo.ino': 'foo.ino',
+				'src/bar.cpp': '../src/bar.cpp',
+				'src/bar.h': '../src/bar.h'
+			});
+		});
+	});
+
+	describe('mapKeys', () => {
+		it('maps the source using mapper', () => {
+			const input = {
+				foo: 'bar'
+			};
+			let result = compilationContext.mapKeys(input, (k, v) => k.toUpperCase());
+			expect(result).to.eql({FOO: 'bar'});
+		});
+	});
+
+	describe('makeAbsolute', () => {
+		it('prefixes filename', () => {
+			expect(compilationContext.makeAbsolute('foo')).to.equal('/foo');
+		});
+	});
+});
